cd686fbf62e243847e805ab996a04d0542c1cf47
Gozel Yazmuhamedova <gozelyazmuhamedowa@gmail.com>
2022-06-23 19:36:49 +0300

    sidebar components

--- src/components/ButtonSignIn.vue
+++ src/components/ButtonSignIn.vue
@@ -1,6 +1,5 @@
 <template>
-  <button
-    class="flex items-center whitespace-nowrap px-2 py-1 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm"
+  <button :class="classes"
   >
     <BaseIcon name="user" class="mr-2 w-7 h-7" />
     Sign In
@@ -13,6 +12,12 @@ import BaseIcon from './BaseIcon.vue'
 export default {
   components: {
     BaseIcon
+  },
+
+  computed: {
+    classes(){
+      return this.$attrs.class || 'flex items-center whitespace-nowrap px-2 py-1 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm'
+    }
   }
 }
 </script>
