cd5a38e3b186820d99039692e1a3ab9608f48f50
Gozel Yazmuhamedova <gozelyazmuhamedowa@gmail.com>
2022-06-26 02:00:23 +0300

    animation of buttons & the dropdown list positioning (x/y) of video blocks

--- src/components/VideoItemInfo.vue
+++ src/components/VideoItemInfo.vue
@@ -5,33 +5,28 @@
       class="mr-3 w-9 h-9 rounded-full"
     />
     <div class="text-sm">
-      <span class=" font-semibold text-gray-800">Video title {{index}} </span>
+      <span class=" font-semibold text-gray-800">Video title {{ index }} </span>
       <div class=" mt-1 flex items-center">
-        <span>Channel name {{index}} </span>
-        <BaseIcon name="checkCircle"  class="w-3.5 h-3.5 ml-1" />
+        <span>Channel name {{ index }} </span>
+        <BaseIcon name="checkCircle" class="w-3.5 h-3.5 ml-1" />
       </div>
       <div v-html="summary"></div>
     </div>
-    <button
-      class="ml-auto -mt-1 p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 hover:outline-none"
-    >
-      <BaseIcon name="dotsVertical" />
-    </button>
+    <VideoItemDropdown />
   </div>
 </template>
 
 <script>
-import BaseIcon from './BaseIcon.vue'
+import VideoItemDropdown from './VideoItemDropdown.vue'
 export default {
   props: ['index'],
-  components: { BaseIcon },
+  components: {  VideoItemDropdown },
 
   computed: {
-    summary() {
-        const days = this.index == 1 ? 'day' : 'days'
-        return `${this.index}K veiws &middot; ${this.index} ${days} ago`
+    summary () {
+      const days = this.index == 1 ? 'day' : 'days'
+      return `${this.index}K veiws &middot; ${this.index} ${days} ago`
     }
-
   }
 }
 </script>
--- src/components/VideoItemThumbnail.vue
+++ src/components/VideoItemThumbnail.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="relative">
     <img :src="`https://picsum.photos/seed/video${index}/720/404`" alt="" />
-    <span
-      class=" opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-0 right-0 bg-black text-white rounded-sm m-2 p-1"
-    >
-    <BaseIcon name="clock" class="w-5 h-5" />
-    </span>
-    <span
-      class="opacity-0 group-hover:opacity-100 bg-opacity-60 absolute top-8 right-0 bg-black text-white rounded-sm m-2 p-1"
-    >
-     <BaseIcon name="menuAlt3" class="w-5 h-5" />
-    </span>
+    <VideoItemThumbnailButton icon="clock" label="Watch later" class="top-0" />
+    <VideoItemThumbnailButton
+      icon="menuAlt3"
+      label="Add to queue"
+      class="top-8"
+    />
     <span
       class="opacity-100 group-hover:opacity-0 duration-500 absolute bottom-0 right-0 bg-black text-white rounded-sm m-1 p-1 text-sm font-semibold"
     >
@@ -20,9 +16,15 @@
 </template>
 
 <script>
-import BaseIcon from './BaseIcon.vue';
+import BaseIcon from './BaseIcon.vue'
+import VideoItemThumbnailButton from './VideoItemThumbnailButton.vue'
+
 export default {
-    props: ["index"],
-    components: { BaseIcon }
+  components: {
+    BaseIcon,
+    VideoItemThumbnailButton
+  },
+
+  props: ['index']
 }
 </script>
