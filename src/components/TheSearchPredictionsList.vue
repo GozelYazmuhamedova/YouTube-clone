42f20848d3171b34bd590e46b635644eda5967c2
Gozel Yazmuhamedova <gozelyazmuhamedowa@gmail.com>
2022-07-06 22:41:38 +0300

    create BaseCheckBox component

--- src/components/TheSearchPredictionsList.vue
+++ src/components/TheSearchPredictionsList.vue
@@ -1,43 +1,33 @@
 <template>
-  <div class="space-y-3 text-black text-sm mt-5">
+  <div class="space-y-2.5 text-black text-sm mt-5">
     <div
       v-for="(label, index) in searchPredictions"
       :key="label"
       class="flex items-center"
     >
-      <input
-        type="checkbox"
-        class="h-5 w-5 cursor-pointer"
+      <BaseCheckBox
         :id="index"
         :value="label"
-        v-model="selectedSearchPredictions"
-      />
-      <label :for="index" class="pl-4 cursor-pointer flex-grow">
-        {{ label }}
-      </label>
+        v-bind="$attrs"
+        v-model="modelValue"
+      >
+       {{ label }}
+      </BaseCheckBox>
     </div>
   </div>
 </template>
 
 <script>
+import BaseCheckBox from './BaseCheckBox.vue'
+
 export default {
+  components: {
+    BaseCheckBox
+  },
+
   props: {
     searchPredictions: Array,
     modelValue: Array
-  },
-
-  emits: ['update:modelValue'],
-
-  data () {
-    return {
-      selectedSearchPredictions: this.modelValue
-    }
-  },
-
-  watch: {
-    selectedSearchPredictions () {
-      this.$emit('update:modelValue', this.selectedSearchPredictions)
-    }
   }
 }
 </script>
